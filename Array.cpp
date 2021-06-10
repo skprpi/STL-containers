d71c11b7c2496388968ee31e1340a05927892496
skprpi <60091575+skprpi@users.noreply.github.com>
2021-06-10 11:40:09 +0300

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -1,58 +1,60 @@
 #include <iostream>
 #include <memory>
+#include <cassert>
 
 using std::cout;
 
-template <class T, size_t size>
-class Array{
+template<class T, size_t size>
+class StaticVector {
 public:
 
-    // copy constructor
-    Array(): data_(new T[size]) {}
+    StaticVector() : data_(new T[size]) {}
 
 
-    Array(const Array<T, size>& data): Array() {
+    StaticVector(const StaticVector<T, size> &data) : StaticVector() {
         for (int i = 0; i < size; i++) {
             data_[i] = data[i];
         }
     }
 
-    const T& operator[] (size_t i) const {
-        return data_[i];
-    }
-
-    T& operator[] (size_t i) {
-        return data_[i];
-    }
-
-    Array& operator= (const Array<T, size>& arr) {
+    StaticVector &operator=(const StaticVector<T, size> &arr) {
         if (&arr == this) {
             return *this;
         }
-        Array<T, size>(arr).swap(*this);
+        StaticVector<T, size>(arr).swap(*this);
     }
 
-    void swap(Array<T, size>& arr) {
+    ~StaticVector() = default;
+
+    const T &operator[](size_t i) const {
+        return data_[i];
+    }
+
+    T &operator[](size_t i) {
+        return data_[i];
+    }
+
+
+    void swap(StaticVector<T, size> &arr) {
         std::swap(arr.data_, data_);
     }
 
-    friend std::ostream& operator<< (std::ostream& out, const Array<T, size>& arr) {
+    friend std::ostream &operator<<(std::ostream &out, const StaticVector<T, size> &arr) {
         for (int i = 0; i < size; i++) {
             out << arr[i] << " ";
         }
-        cout << "\n";
+        out << std::endl;
         return out;
     }
 
-    friend std::istream& operator>> (std::istream& in, Array<T, size>& arr) {
+    friend std::istream &operator>>(std::istream &in, StaticVector<T, size> &arr) {
         for (int i = 0; i < size; i++) {
             in >> arr[i];
         }
-        cout << "\n";
         return in;
     }
 
-    Array<T, size>& operator += (const Array<T, size>& arr) {
+    StaticVector<T, size> &operator+=(const StaticVector<T, size> &arr) {
         for (int i = 0; i < size; ++i) {
             data_[i] += arr[i];
         }
@@ -60,7 +62,7 @@ public:
     }
 
     // don't work
-    Array<T, size>& operator -= (const Array<T, size>& arr) {
+    StaticVector<T, size> &operator-=(const StaticVector<T, size> &arr) {
         for (int i = 0; i < size; ++i) {
             data_[i] -= arr[i];
         }
@@ -69,8 +71,8 @@ public:
 
 
     // postfix increment    something wrong !!! - have some warnings
-    Array<T, size> operator++(int) {
-        Array<T, size> res = *this;
+    StaticVector<T, size> operator++(int) {
+        StaticVector<T, size> res = *this;
         for (int i = 0; i < size; ++i) {
             data_[i] += 1;
         }
@@ -78,7 +80,7 @@ public:
     }
 
     // prefix increment
-    Array<T, size>& operator++() {
+    StaticVector<T, size> &operator++() {
         for (int i = 0; i < size; ++i) {
             data_[i] += 1;
         }
@@ -86,8 +88,8 @@ public:
     }
 
 
-    Array<T, size> operator--(int) {
-        Array<T, size> res = *this;
+    StaticVector<T, size> operator--(int) {
+        StaticVector<T, size> res = *this;
         for (int i = 0; i < size; ++i) {
             data_[i] -= 1;
         }
@@ -95,46 +97,45 @@ public:
     }
 
     // prefix increment
-    Array<T, size>& operator--() {
+    StaticVector<T, size> &operator--() {
         for (int i = 0; i < size; ++i) {
             data_[i] -= 1;
         }
         return *this;
     }
 
-    ~Array() = default;
 
 private:
     std::unique_ptr<T[]> data_;
 };
 
-template <typename T, size_t size>
-Array<T, size> operator-(const Array<T, size>& arr) {
-    Array<T, size> tmp = Array<T, size>(arr);
+template<typename T, size_t size>
+StaticVector<T, size> operator-(const StaticVector<T, size> &arr) {
+    StaticVector<T, size> tmp = StaticVector<T, size>(arr);
     for (int i = 0; i < size; ++i) {
         tmp[i] *= -1;
     }
     return tmp;
 }
 
-template <typename T, size_t size>
-Array<T, size> operator + (const Array<T, size>& arr1, const Array<T, size>& arr2) {
-    Array<T, size> tmp = arr1;
+template<typename T, size_t size>
+StaticVector<T, size> operator+(const StaticVector<T, size> &arr1, const StaticVector<T, size> &arr2) {
+    StaticVector<T, size> tmp = arr1;
     tmp += arr2;
     return tmp;
 }
 
-template <typename T, size_t size>
-Array<T, size> operator - (const Array<T, size>& arr1, const Array<T, size>& arr2) {
-    Array<T, size> tmp = arr1;
+template<typename T, size_t size>
+StaticVector<T, size> operator-(const StaticVector<T, size> &arr1, const StaticVector<T, size> &arr2) {
+    StaticVector<T, size> tmp = arr1;
     tmp -= arr2;
     return tmp;
 }
 
 
-template <typename T, size_t size>
-bool operator<(const Array<T, size>& arr1, const Array<T, size>& arr2)  {
-    for (int i = 0; i < size; i++) {
+template<typename T, size_t size>
+bool operator<(const StaticVector<T, size> &arr1, const StaticVector<T, size> &arr2) {
+    for (size_t i = 0; i < size; ++i) {
         if (arr1[i] != arr2[i]) {
             return arr1[i] < arr2[i];
         }
@@ -142,34 +143,34 @@ bool operator<(const Array<T, size>& arr1, const Array<T, size>& arr2)  {
     return false;
 }
 
-template <typename T, size_t size>
-bool operator>(const Array<T, size>& arr1, const Array<T, size>& arr2)  {
+template<typename T, size_t size>
+bool operator>(const StaticVector<T, size> &arr1, const StaticVector<T, size> &arr2) {
     return arr2 < arr1;
 }
 
-template <typename T, size_t size>
-bool operator==(const Array<T, size>& arr1, const Array<T, size>& arr2)  {
+template<typename T, size_t size>
+bool operator==(const StaticVector<T, size> &arr1, const StaticVector<T, size> &arr2) {
     return !(arr2 < arr1) && !(arr1 < arr2);
 }
 
-template <typename T, size_t size>
-bool operator!=(const Array<T, size>& arr1, const Array<T, size>& arr2)  {
+template<typename T, size_t size>
+bool operator!=(const StaticVector<T, size> &arr1, const StaticVector<T, size> &arr2) {
     return !(arr1 == arr2);
 }
 
-template <typename T, size_t size>
-bool operator<=(const Array<T, size>& arr1, const Array<T, size>& arr2)  {
-    return (arr1 == arr2) || (arr1 < arr2);
+template<typename T, size_t size>
+bool operator<=(const StaticVector<T, size> &arr1, const StaticVector<T, size> &arr2) {
+    return !(arr1 > arr2);
 }
 
-template <typename T, size_t size>
-bool operator>=(const Array<T, size>& arr1, const Array<T, size>& arr2)  {
-    return (arr1 == arr2) || (arr1 > arr2);
+template<typename T, size_t size>
+bool operator>=(const StaticVector<T, size> &arr1, const StaticVector<T, size> &arr2) {
+    return !(arr1 < arr2);
 }
 
 
-template <typename T, size_t size>
-T operator*(const Array<T, size>& arr1, const Array<T, size>& arr2)  {
+template<typename T, size_t size>
+T operator*(const StaticVector<T, size> &arr1, const StaticVector<T, size> &arr2) {
     T res = 0;
     for (int i = 0; i < size; ++i) {
         res += arr1[i] * arr2[i];
@@ -178,15 +179,14 @@ T operator*(const Array<T, size>& arr1, const Array<T, size>& arr2)  {
 }
 
 
-
 int main() {
-    Array<int, 4> arr1;
+    StaticVector<int, 4> arr1;
     arr1[0] = 100;
     arr1[1] = 3;
     arr1[2] = 12;
     arr1[3] = -15;
     std::cout << arr1;
-    Array<int, 4> arr2 = arr1;
+    StaticVector<int, 4> arr2 = arr1;
     arr2[1] = 1;
     std::cout << arr2;
 
@@ -203,11 +203,12 @@ int main() {
     cout << arr2;
     cout << --arr2;
     cout << arr2--;
-    cout << arr2 ++;
+    cout << arr2++;
 
     cout << arr2 * arr1;
 
-    Array<int, 4> arr3;
+    StaticVector<int, 4> arr3;
     std::cin >> arr3;
     cout << arr3;
+    return 0;
 }
