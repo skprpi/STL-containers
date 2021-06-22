2dce33603d4ad4faba57bf3f029d97be9534c26d
skprpi <60091575+skprpi@users.noreply.github.com>
2021-06-22 13:59:14 +0300

    Update List.cpp

--- List.cpp
+++ List.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <algorithm>
 
 
 //Дз.
@@ -30,20 +31,29 @@ class List {
 public:
     class Node;
     class iterator {
-        friend List;
     public:
+        friend List<T>;
+
+        using iterator_category = std::bidirectional_iterator_tag;
+        using difference_type   = long long;
+        using value_type        = T;
+        using pointer           = T*;  // or also value_type*
+        using reference         = T&;  // or also value_type&
+
+        explicit iterator(Node* other): now_node(other) {}
+
         T& operator*() {
             return now_node->data;
         }
 
         iterator& operator++() {
             now_node = now_node->next;
-            return now_node;
+            return *this;
         }
 
         iterator& operator--() {
             now_node = now_node->prev;
-            return now_node;
+            return *this;
         }
 
         bool operator==(const iterator& other) {
@@ -54,71 +64,129 @@ public:
             return now_node != other.now_node;
         }
 
+        T& operator*() const {
+            return now_node->data;
+        }
 
 
-    private:
 
-        std::unique_ptr<Node> now_node;
+        Node* now_node;
     };
 
     class Node{
     public:
-        std::weak_ptr<Node> next;
-        std::weak_ptr<Node> prev;
+        Node* next;
+        Node* prev;
         T data;
 
         // ERROR!
         Node(): next(nullptr), prev(nullptr) {}
 
-        Node(std::weak_ptr<Node> prev, std::weak_ptr<Node> next, T& data): next(next), prev(prev), data(data) {}
+        explicit Node(Node* n1): prev(n1->prev), next(n1->next), data(n1->data) {}
+
+        Node& operator=(const Node* n1) {
+            prev = n1->prev;
+            next = n1->next;
+            data = n1->data;
+            return *this;
+        }
+
+        Node(Node* prev, Node* next, T& data): next(next), prev(prev), data(data) {}
     };
 
-
-    void insert(iterator& it, T& data) {
-        if (size_++ == 0) {
-            begin_ptr = new Node(nullptr, end_ptr, data);
-            end_ptr.prev = begin_ptr;
-            return;
-        }
-        std::weak_ptr<Node> new_node = new Node(it.now_node, it.now_node.next, data);
-        it.now_node.next = new_node;
-        it.now_node.next.now_node.prev = new_node;
+    iterator begin() {
+        return iterator(begin_ptr);
     }
 
-    void erase(iterator& it) {
+    iterator end() {
+        return iterator(end_ptr);
+    }
+
+
+    void insert(iterator& it, T data) {
+        if (size_++ == 0) {
+            begin_ptr = new Node(nullptr, end_ptr, data);
+            end_ptr->prev = begin_ptr;
+            return;
+        }
+        Node* new_node = new Node(it.now_node, it.now_node->next, data);
+        it.now_node->next->prev = new_node;
+        it.now_node->next = new_node;
+//        it.now_node->next->now_node.prev = new_node;
+
+    }
+
+    void erase(iterator it) {
         --size_;
         if (it.now_node == begin_ptr) {
-            it.now_node = it.now_node.next;
+            begin_ptr = begin_ptr->next;
+            begin_ptr->prev = nullptr;
+            delete it.now_node;
             return;
         }
         if (it.now_node == end_ptr) {
-            it.now_node = it.now_node.prev;
+            end_ptr = end_ptr->prev;
+            end_ptr->next = nullptr;
+            delete it.now_node;
             return;
         }
-        it.now_node.prev.next = it.now_node.next;
-        it.now_node.next.prev = it.now_node.prev;
+        it.now_node->prev->next = it.now_node->next;
+        it.now_node->next->prev = it.now_node->prev;
     }
 
     size_t size() const {
         return size_;
     }
 
-    // ERROR!
     List(): begin_ptr(new Node()), size_(0), end_ptr(begin_ptr) {}
 
 private:
-    std::weak_ptr<Node> begin_ptr;
-    std::weak_ptr<Node> end_ptr;
+    Node* begin_ptr;
+    Node* end_ptr;
     size_t size_;
 };
 
 
 int main() {
     List<int> list;
+    auto it = list.begin();
+    list.insert(it, 100);
+    int a = 2;
+    it = list.begin();
+    list.insert(it, a);
+    list.insert(it, a*10);
+    auto it2 = list.end();
+    it = list.begin();
+
+    for (int & i : list) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    if (std::find(list.begin(), list.end(), 20) != list.end()) {
+        std::cout << "Here1!" << std::endl;
+    }
+
+    if (std::find(list.begin(), list.end(), 101) != list.end()) {
+        std::cout << "Here2!" << std::endl;
+    }
+
+    if (std::find(list.begin(), list.end(), 23) != list.end()) {
+        std::cout << "Here3!" << std::endl;
+    }
+
+    list.erase(list.begin());
+
+
+    for (int & i : list) {
+        std::cout << i << " ";
+    }
+
+
+    list.erase(list.end());
+    std::cout << std::endl;
+
+    for (int & i : list) {
+        std::cout << i << " ";
+    }
 }
-
-
-
-
-
-
